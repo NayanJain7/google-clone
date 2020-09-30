50fd2dcb8218e5a04fab7731c7d3b4357e23b10e
Nayan Jain <71127472+NayanJain7@users.noreply.github.com>
2020-09-30 18:23:56 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,34 +5,36 @@ import AppsIcon from "@material-ui/icons/Apps";
 import { Avatar } from "@material-ui/core";
 import Search from '../Search';
 
-function Home() {
-  return (
-    <div cl="home">
-      <div className="home__header ">
-        <div className="home__headerLeft">
-          <Link to='/about'>About</Link>
-          <Link to='/store'>Store</Link>
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home_header">
+                <div className="home_headerLeft">
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className="home_headerRight">
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+                    <AppIcon />
+                    <Avatar />
+                </div>
+            </div>
+
+            <div className="home_body">
+                <img
+                    src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+                    alt="Google"
+                />
+                <div className="home_inputContainer">
+                    <Search />
+                </div>
+            </div>
+
+
+
         </div>
-
-        <div className="home__headerRight">
-          <Link to='/gmail'>Gmail</Link>
-          <Link to='/images'>Images</Link>
-          <AppsIcon />
-          <Avatar />
-        </div>
-      </div>
-
-      <div className="home__body">
-        <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-          alt="" />
-      </div>
-
-      <div className="home__inputContainer">
-        <Search />
-      </div>
-
-    </div>
-  )
-}
-
-export default Home
+    );
+};
+export default Home;
